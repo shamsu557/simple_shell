162ea86600c01a710dcdb392d9219c39f736f062
shamsu557 <1440shamsusabo@gmail.com>
2023-02-21 19:09:21 -0800

    group proiect

---
+++ _atoi.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
